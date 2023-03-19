81fd871acc1890b86eacc890f1de1022600bdc6f
ojknation <ojknation@gmail.com>
2023-03-19 23:17:50 +0100

    remove lint on start

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), eslint({ lintOnStart: true })],
+  plugins: [react(), svgr(), eslint()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
