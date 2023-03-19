a72781f666127a709c757296452c2a81a45216bc
ojknation <ojknation@gmail.com>
2023-03-19 22:20:04 +0100

    make lint error fail build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import svgr from "vite-plugin-svgr"
+import eslint from "vite-plugin-eslint"
+
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), eslint({ lintOnStart: true })],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
