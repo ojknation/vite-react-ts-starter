334fd538d3be195f8371a5d824a25cabe1f31ff9
Adenola desmond <56209529+locksiDesmond@users.noreply.github.com>
2023-04-09 15:08:33 +0100

    fix: mutation doesn't return error and loading state doesn't update.

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,31 +1,36 @@
-import { theme } from "@/theme"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from '@/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ChakraProvider } from '@chakra-ui/react';
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-const reactQueryConfig = {
+const queryConfig = {
   refetchOnWindowFocus: false,
   retry: true,
-}
+};
+
+const mutationConfig = {
+  refetchOnWindowFocus: false,
+  retry: false,
+};
 
 const defaultOptions = {
-  queries: reactQueryConfig,
-  mutations: reactQueryConfig,
-}
+  queries: queryConfig,
+  mutations: mutationConfig,
+};
 
 const queryClient = new QueryClient({
   defaultOptions,
-})
+});
 
 const Providers = (props: Props) => {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>{props.children}</ChakraProvider>
     </QueryClientProvider>
-  )
-}
+  );
+};
 
-export default Providers
+export default Providers;
