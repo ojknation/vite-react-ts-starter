679e0ac1de8ce0256cd1b3f3dedd576aed6af86d
Adenola desmond <56209529+locksiDesmond@users.noreply.github.com>
2023-04-17 16:37:05 +0100

    chore: fix eslint issue

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,36 +1,36 @@
-import { theme } from '@/theme';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from "@/theme"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ChakraProvider } from "@chakra-ui/react"
 
 type Props = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
-const queryConfig = {
+const reactQueryConfig = {
   refetchOnWindowFocus: false,
   retry: true,
-};
+}
 
-const mutationConfig = {
+const mutationQueryConfig = {
   refetchOnWindowFocus: false,
   retry: false,
-};
+}
 
 const defaultOptions = {
-  queries: queryConfig,
-  mutations: mutationConfig,
-};
+  queries: reactQueryConfig,
+  mutations: mutationQueryConfig,
+}
 
 const queryClient = new QueryClient({
   defaultOptions,
-});
+})
 
 const Providers = (props: Props) => {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>{props.children}</ChakraProvider>
     </QueryClientProvider>
-  );
-};
+  )
+}
 
-export default Providers;
+export default Providers
